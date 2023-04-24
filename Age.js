b506653e1593e0fcda812b911cd29c6bb617b593
EvansRobbie <evansitoden94@gmail.com>
2023-04-24 22:20:37 +0300

    question 1 commit

---
+++ Age.js
@@ -0,0 +1,23 @@
+// console.log('Helloo')
+
+// Given an array of objects, each object representing a person with a name and age property, write a function that returns a new array containing the names of all people who are 18 years old or older.
+// const people = [
+// { name: 'Alice', age: 17 },
+// { name: 'Eunice', age: 22 },
+// { name: 'Charlie', age: 14 },
+// { name: 'Max', age: 19 },
+// ];
+// I'll use the .filter and .map method to solve it
+
+const Adult = (people) =>{
+    return people.filter(person => person.age >= 18).map(person => person.name)
+}
+const people = [
+{ name: 'Alice', age: 17 },
+{ name: 'Eunice', age: 22 },
+{ name: 'Charlie', age: 14 },
+{ name: 'Max', age: 19 },
+];
+
+const nameOfAdults = Adult(people)
+console.log(nameOfAdults) //[ 'Eunice', 'Max' ]
